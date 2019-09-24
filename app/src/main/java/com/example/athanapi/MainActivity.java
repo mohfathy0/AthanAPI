fd677e895c38a35e36c12f8ed4558063d304cebf
mohfathy0 <moh.fathy0@yahoo.com>
2019-09-24 16:31:21 +0200

    Adding prayer time identification

--- app/src/main/java/com/example/athanapi/MainActivity.java
+++ app/src/main/java/com/example/athanapi/MainActivity.java
@@ -3,10 +3,21 @@ package com.example.athanapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -17,12 +28,13 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView=findViewById(R.id.textviewResponse);
-        editText=findViewById(R.id.editTextAddress);
+        textView = findViewById(R.id.textviewResponse);
+        editText = findViewById(R.id.editTextAddress);
 
 
     }
@@ -33,34 +45,135 @@ public class MainActivity extends AppCompatActivity {
         call.enqueue(new Callback<PrayerTime>() {
             @Override
             public void onResponse(Call<PrayerTime> call, Response<PrayerTime> response) {
-                SendLog(response.message()+" \n"+response.body().getData().getTimings().getFajr());
-                textView.setText("Response message: "+ response.message()+"\n"+
-                        "Response Code: "+ response.code()+"\n"+"\n"+
-                        "Fajr: "+ response.body().getData().getTimings().getFajr()+"\n"+
-                        "Dhuhr: "+ response.body().getData().getTimings().getDhuhr()+"\n"+
-                        "Asr: "+ response.body().getData().getTimings().getAsr()+"\n"+
-                        "Maghrib: "+ response.body().getData().getTimings().getMaghrib()+"\n"+
-                        "Isha: "+ response.body().getData().getTimings().getIsha()
+                SendLog(response.message() + " \n" + response.body().getData().getTimings().getFajr());
+                StringBuilder sbf= new StringBuilder("");
+
+                SimpleDateFormat current = new SimpleDateFormat("HH:mm");
+
+                Date currentDate = new Date();
+
+                ArrayList<String> prayerTimes = new ArrayList<>();
+                prayerTimes.add(response.body().getData().getTimings().getFajr() );
+                prayerTimes.add(response.body().getData().getTimings().getDhuhr() );
+                prayerTimes.add(response.body().getData().getTimings().getAsr() );
+                prayerTimes.add(response.body().getData().getTimings().getMaghrib() );
+                prayerTimes.add(response.body().getData().getTimings().getIsha() );
 
 
-                );
+
+
+                Date PrayerTime = new Date();
+                DateFormat Prayer = new SimpleDateFormat("HH:mm");
+               Prayer.setLenient(true);
+                boolean prayertimefound=false;
+               for (int x =0;x<prayerTimes.size()-1;x++){
+
+
+
+                      if( CompareTime(prayerTimes,x)){
+               //        if (Prayer.parse(prayerTimes.get(x)).compareTo(currentDate)>0 &&Prayer.parse(prayerTimes.get(x+1)).compareTo(currentDate)<0){
+                      // if (currentDate.compareTo(Prayer.parse(prayerTimes.get(x)))>0 && currentDate.compareTo(Prayer.parse(prayerTimes.get(x+1)))<0 ){
+
+                           switch (prayerTimes.indexOf(prayerTimes.get(x))){
+                               case 0:
+                                   sbf.append("It is time to pray Fajer\n");
+                                   prayertimefound=true;
+                                   break;
+                               case 1:
+                                   sbf.append("It is time to pray Duhr\n");
+                                   prayertimefound=true;
+                                   break;
+                               case 2:
+                                   sbf.append("It is time to pray Asr\n");
+                                   prayertimefound=true;
+                                   break;
+                               case 3:
+                                   sbf.append("It is time to pray Maghrib\n");
+                                   prayertimefound=true;
+                                   break;
+                               case 4:
+                                   sbf.append("It is time to pray Ishaa\n");
+                                   prayertimefound=true;
+                                   break;
+                           }
+
+                          if (prayertimefound ){
+                              break;
+                          }
+                       }
+
+
+               }
+
+                String str = "08:03";
+
+
+                try {
+                    PrayerTime = Prayer.parse(str);
+                } catch (Exception e) {
+
+                }
+
+
+                String placeHolder = "\nResponse message: " + response.message() + "\n" +
+                        "Response Code: " + response.code() + "\n" + "\n" +
+                        "Fajr: " + response.body().getData().getTimings().getFajr() + "\n" +
+                        "Dhuhr: " + response.body().getData().getTimings().getDhuhr() + "\n" +
+                        "Asr: " + response.body().getData().getTimings().getAsr() + "\n" +
+                        "Maghrib: " + response.body().getData().getTimings().getMaghrib() + "\n" +
+                        "Isha: " + response.body().getData().getTimings().getIsha();
+                sbf.append(placeHolder);
+                textView.setText(sbf);
             }
 
             @Override
             public void onFailure(Call<PrayerTime> call, Throwable t) {
                 SendLog(t.getMessage());
-            textView.setText(t.getMessage());
+                textView.setText(t.getMessage());
             }
         });
 
     }
 
-    public void SendLog(String message){
-        Log.i("mylog_MainAcritivity",message);
+    public void SendLog(String message) {
+        Log.i("mylog_MainAcritivity", message);
     }
 
-    public void onClick(View view){
+    public void onClick(View view) {
         textView.setText("");
         GetAthanTimeByLocation(editText.getText().toString());
+    }
+
+
+    public boolean CompareTime(ArrayList<String> prayerTime, int x){
+        if (x<5) {
+            Calendar Now = Calendar.getInstance();
+            Now.setTime(new Date());
+            Now.set(1970, Calendar.JANUARY, 1);
+
+            Calendar prayertimeCalender = Calendar.getInstance();
+            Calendar prayertimeCalenderNext = Calendar.getInstance();
+            DateFormat Prayer = new SimpleDateFormat("HH:mm");
+            Date d = null;
+            Date d2 = null;
+            try {
+                d = Prayer.parse(prayerTime.get(x));
+                d2 = Prayer.parse(prayerTime.get(x + 1));
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            prayertimeCalender.setTime(d);
+
+            prayertimeCalenderNext.setTime(d2);
+
+            // if (Prayer.parse(prayerTimes.get(x)).compareTo(currentDate)>0 &&Prayer.parse(prayerTimes.get(x+1)).compareTo(currentDate)<0){
+            return  (Now.compareTo(prayertimeCalender) > 0 && Now.compareTo(prayertimeCalenderNext) < 0);
+
+
+        }else{
+            return true;
+        }
+
     }
 }
